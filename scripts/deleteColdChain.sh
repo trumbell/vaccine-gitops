fcee92442db9c594ba57bafc09cf8de8b3529525
Tim Rumbell <thrumbel@us.ibm.com>
2021-10-04 13:55:05 -0400

    Turned off 'anomaly detection' through CP4D/WML

--- scripts/deleteColdChain.sh
+++ scripts/deleteColdChain.sh
@@ -2,3 +2,10 @@
 #!/bin/bash
 scriptDir=$(dirname $0)
 oc delete -k ${scriptDir}/../apps/cold-chain-use-case
+oc delete -k ${scriptDir}/../apps/freezer-mgr
+oc delete -k ${scriptDir}/../environments/strimzi
+oc delete kafkauser scram-user
+oc delete secret freezer-mgr-secret
+oc delete secret reefer-monitoring-agent-secret
+oc delete secret reefer-simul-secret
+oc delete serviceaccount vaccine-runtime
